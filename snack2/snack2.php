151e71de9ecddee24c755252ca0c515d1779bb0d
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-01-24 16:58:43 +0100

    added form and if with isset

--- snack2/snack2.php
+++ snack2/snack2.php
@@ -1,5 +1,9 @@
 <?php 
-
+    if (isset($_GET['name']) && (isset($_GET['email'])) && (isset($_GET['age']))) {
+        $name = $_GET['name'];
+        $email = $_GET['email'];
+        $age = $_GET['age'];
+    }
 ?>
 
 <!DOCTYPE html>
@@ -10,8 +14,15 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="">
-        
+    <h1>Snack 2</h1>
+    <form action="./snack2.php" method="GET">
+        <label for="">Name:</label>
+        <input type="text" name="name">
+        <label for="">Email:</label>
+        <input type="text" name="email">
+        <label for="">Age:</label>
+        <input type="text" name="age">
+        <button type="submit">Send</button>
     </form>
 </body>
 </html>
