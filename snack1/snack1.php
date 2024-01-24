099f1dd1cfe5b7749c0a1c6c87eb7414de59ff06
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-01-24 15:29:18 +0100

    added array in the snack1

--- snack1/snack1.php
+++ snack1/snack1.php
@@ -1,9 +1,31 @@
+<?php 
+    $partite =
+    [
+        [
+            'Roma' => '90',
+            'Milano' => '80',
+        ],
+        [
+            'Bologna' => '70',
+            'Firenze' => '60',
+        ],
+        [
+            'Trieste' => '40',
+            'Salerno' => '20',
+        ],
+        [
+            'La Spezia' => '30',
+            'Genova' => '50',
+        ]
+    ]
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Basket Results</title>
 </head>
 <body>
     
